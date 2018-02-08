1cfc4f24afd33cc7e6851b7353c025bff64815c6
nwilliamfeng <nwilliamfeng@163.com>
2018-02-08 21:24:57 +0800

    commit 180208b

---
+++ src/login/routers/LoginRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+// 此方法属于包装组合模式
+export const LoginRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        localStorage.getItem('user')
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    )} />
+)
