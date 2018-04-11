6affb36e59614600f0c86d61579db0ec2b9710b3
nwilliamfeng <nwilliamfeng@163.com>
2018-04-11 17:36:48 +0800

    commit 180411a

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -9,7 +9,7 @@ import { Chat } from '../../chat/components';
 import { homeActions } from '../actions';
 import SplitPane from 'react-split-pane';
 import { ChatRegion } from './ChatRegion'
-require('../../assets/styles/react-split-pane.css');
+require('../../assets/styles/grid.css');
 
 
 const navibarStyle = {
@@ -29,26 +29,55 @@ class HomePage extends Component {
     }
 
     render() {
+        const a = [];
+        for (var i = 0; i < 20; i++) {
+            a.push(1);
+        }
 
         return (
             // <SplitPane split='vertical' allowResize={false} pane1Style={navibarStyle}  defaultSize={60}  >
             //     <Navibar/>
             //     <ChatRegion/>
             // </SplitPane>
-            <div style={{ float: 'left', left: 0, right: 0, position: 'absolute' }}>
-                
-                <Navibar/>
-                    {/* <div style={{background:'green' ,width:'100%',height:'calc(100% - 60px)'}}>
-                    </div>
-                    <div>
-                        <p style={{color:'red'}}>32</p>
-                    </div> */}
-                
-                <div style={{ float: 'left',position:'fixed', left:60,overflow:'hidden',height:'calc(100%)'   }}>
-                       <ChatRegion />  
-                        
-                </div>
+            // <div style={{ float: 'left', left: 0, right: 0, position: 'absolute' }}>
 
+            //     <Navibar/>
+
+            //     <div style={{ float: 'left',position:'fixed', left:60,overflow:'hidden',height:'calc(100%)'   }}>
+            //            <ChatRegion />  
+
+            //     </div>
+
+            // </div>
+
+            // <div className='row' style={{height:'100vh', overflowY:'hidden'}} >
+            //     <div className='col-xs-1' style={{ background: 'red', height:'100%'  }}>
+            //     </div>
+            //     <div className='col-xs-2' style={{ background: 'yellow', height:'100%'   }}>
+            //     </div>
+            //     <div className='col-xs-9' style={{   background: 'green',  }}>
+            //         <div style={{ maxHeight:'100vh',height:'100vh',  overflowY: 'auto', }} >
+            //             {a.map((item) => (
+            //                 <p>abc</p>
+            //             ))}
+            //         </div>
+            //     </div>
+            // </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-fixed-240">Fixed 240px</div>
+                    <div className="col-fixed-160">Fixed 160px</div>
+                    <div className="col-md-12 col-offset-400">
+                        <div className="row">
+                        <Scrollbars style={{  height: 'calc(100% - 52px)', position: 'absolute', }}>
+                        {a.map((item) => (
+                            <p>abc</p>
+                        ))}
+                        </Scrollbars>
+            </div>
+                    </div>
+                </div>
             </div>
 
         );
