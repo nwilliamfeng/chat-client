bfc3d6e949bb57a218a013c274d85030515ca452
nwilliamfeng <nwilliamfeng@163.com>
2018-11-11 22:46:55 +0800

    commit181110

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -8,6 +8,7 @@ import { CompositList, CustomerSearch } from '../../customers/components'
 import styled from 'styled-components'
 import Alert from 'react-s-alert'
 import { withScroll } from '../../controls'
+import Draggable from 'react-draggable';
 
 /**
  * 背景板
@@ -35,7 +36,6 @@ const ListRegion = styled.div`
     max-width:240px;
     display:flex;
     flex-direction:column;`
-    
 
 /**
  * 列表容器
@@ -54,35 +54,54 @@ const NavibarContainer = styled.div`
     width:60px;
     background:#2A2D32;`
 
+const OutSpliterDiv = styled.div`
+ width:8px;
+ height:100%;
+ background-color:transparent;
+ flex-direction:row;
+ align-content:center;
+ justify-content:center;
+ display:flex;
+`
+
+const InnerSpliterDiv = styled.div`
+ width:1px;
+ height:100%;
+ background:lightgray;
+`
+
+const Spliter = (props) => <OutSpliterDiv {...props}><InnerSpliterDiv /> </OutSpliterDiv>
+
+
 /**
  * 搜索框容器
  */
 const SearchBoxContainer = styled.div`
    margin:12px 5px 5px 10px;`
 
-const DetailContainer = styled.div`
-    padding-left:250px;
-    z-index:0;
-    background: #F5F5F5;
-    overflow-y: hidden;
-    height: 100vh;`
+// const ChatContainer = styled.div`
+//     flex:0 1 auto;
+//     width:${props=>`1200px - ${props.offset}px)`}
+//     background-color:#F5F5F5;`
 
 const ChatContainer = styled.div`
-    flex:0 1 100%;
-    background-color:red;
-`
+ 
+    width:${props => `${props.offset}px`};
+    background-color:#F5F5F5;`
 
 
 const ExtendContainer = styled.div`
-    float: left;
-    width:auto;
-    background:#F5F5F5;
-    margin-left:0px;
-    margin-right:0px;
+     width:300px;
+    display:flex;
+    background:#F5F5F5; 
     padding-left:5px;
-    padding-right:5px;   
+    padding-right:5px;  
+    align-items: center; 
+    color:gray;
+    font-size:20px;
+    justify-content: center;
     border-left: lightgray solid 1px;
-    height: 100%;`
+    `
 
 const MainContainer = styled.div`
     display:flex;
@@ -98,6 +117,7 @@ class HomePage extends Component {
         super(props)
         this.state = {
             modalIsOpen: false,
+            chatRegionWidth: 600,
         }
     }
 
@@ -113,81 +133,69 @@ class HomePage extends Component {
         this.setState({ modalIsOpen: false });
     }
 
+    handleDrag = (e, data) => {
+         //  console.log('Data: ', data);
+        const { chatRegionWidth } = this.state
+         this.setState({ chatRegionWidth: chatRegionWidth + data.deltaX })
+    };
 
     render() {
-        const { page, error } = this.props;
-        if (error != null) {
-            Alert.success('Test message stackslide effect!', {
-                position: 'top-right',
-                effect: 'stackslide'
-            });
-        }
-        return (
-            <React.Fragment>
-                <Background img={BackgroundImg} />
-                <MainContainer>
-                    <NavibarContainer>
-                        <Navibar />
-                    </NavibarContainer>
-                    <ListRegion  >
-                        <SearchBoxContainer>
-                            <CustomerSearch />
-                        </SearchBoxContainer>
-                        <ListContainerDiv>
-                            <ListContainer>
-                                {page === pageType.CHAT && <ChatList />}
-                                {page === pageType.CUSTOMER_LIST && <CompositList />}
-                            </ListContainer>
-                        </ListContainerDiv>
-                    </ListRegion>
-                    <ChatContainer>
-                        {/* <Chat /> */}
-                    </ChatContainer>
+        const { page, error } = this.props
+        const { chatRegionWidth } = this.state
+     //   console.log('render:' + chatRegionWidth);
+        // if (error != null) {
+        //     Alert.success('Test message stackslide effect!', {
+        //         position: 'top-right',
+        //         effect: 'stackslide'
+        //     })
+        // }
+        return <React.Fragment>
+            <Background img={BackgroundImg} />
+            <MainContainer>
+                <NavibarContainer>
+                    <Navibar />
+                </NavibarContainer>
+                <ListRegion  >
+                    <SearchBoxContainer>
+                        <CustomerSearch />
+                    </SearchBoxContainer>
+                    <ListContainerDiv>
+                        <ListContainer>
+                            {page === pageType.CHAT && <ChatList />}
+                            {page === pageType.CUSTOMER_LIST && <CompositList />}
+                        </ListContainer>
+                    </ListContainerDiv>
+                </ListRegion>
+                <ChatContainer offset={chatRegionWidth}>
+                    <Chat />
+                </ChatContainer>
+                <Draggable
+                    axis="x"
 
+                    onStart={this.handleStart}
+                    onDrag={this.handleDrag}
+                    onStop={this.handleStop}>
+                    <Spliter>
 
+                    </Spliter>
+                </Draggable>
+                <ExtendContainer>{'扩展面板'}</ExtendContainer>
+            </MainContainer>
 
-                    {/* <ListRegion>
-                        <SearchBoxContainer>
-                            <CustomerSearch />
-                        </SearchBoxContainer>
-                        <ListContainerDivWithAutoSize isAbsolute={true}>
-                            <ListContainer>
-                                {page === pageType.CHAT && <ChatList />}
-                                {page === pageType.CUSTOMER_LIST && <CompositList />}
-                            </ListContainer>
-                        </ListContainerDivWithAutoSize>
-                    </ListRegion>
-
-                    <DetailContainer>
-                        <ChatContainer >
-                            <Chat />
-                        </ChatContainer>
-                        <ExtendContainer >
-                        </ExtendContainer>
-
-                    </DetailContainer> */}
-
-
-
-                </MainContainer>
-                {/* <Alert stack={true} timeout={3000} /> */}
-            </React.Fragment>
-
-        );
+            {/* <Alert stack={true} timeout={3000} /> */}
+        </React.Fragment>
     }
 }
 
-
 function mapStateToProps(state) {
-    const states = state.home;
-    const { error } = state.system;
-    return { ...states, error };
+    const states = state.home
+    const { error } = state.system
+    return { ...states, error }
 }
 
-
-const page = connect(mapStateToProps)(HomePage);
+const page = connect(mapStateToProps)(HomePage)
 
 /**
  * 主页
  */
-export { page as HomePage }; 
+export { page as HomePage }
